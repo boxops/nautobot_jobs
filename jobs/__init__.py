7dc7b9258ff3d65e1f7eecf8f56e6dad2348b335
Bal <olahbalazs@outlook.com>
2024-06-05 14:07:42 +0100

    Update __init__.py
    Add ExampleDataSource

--- jobs/__init__.py
+++ jobs/__init__.py
@@ -1,7 +1,9 @@
 """The __init__.py module is required for Nautobot to load the jobs via Git."""
 
 from .get_version import GetShowVersion
+from .example_ssot import ExampleDataSource
 
 __all__ = [
     "GetShowVersion",
+    "ExampleDataSource",
 ]
