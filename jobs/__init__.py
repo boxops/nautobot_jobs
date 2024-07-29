84ebf0170a6c2f1be571b1373cb5f32ef16adf61
Bal <olahbalazs@outlook.com>
2024-07-29 13:19:50 +0100

    test

--- jobs/__init__.py
+++ jobs/__init__.py
@@ -1,9 +1,3 @@
 """The __init__.py module is required for Nautobot to load the jobs via Git."""
 
-from .get_version import GetShowVersion
-from .example_ssot import ExampleDataSource
-
-__all__ = [
-    "GetShowVersion",
-    "ExampleDataSource",
-]
+from . import get_version
